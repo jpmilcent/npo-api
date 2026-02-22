17d6f0752b56145a0634263b7731aa7ac1eba99b
Jean-Pascal MILCENT <jpm@clapas.org>
2026-02-23 00:29:05 +0100

    test: add catch-all health webservices test

--- tests/modules/health/test_routes.py
+++ tests/modules/health/test_routes.py
@@ -1,6 +1,7 @@
 from unittest.mock import AsyncMock, patch
 
 from fastapi import status
+from tests.constants import ERROR_HEALTH_WEBSERVICE_NOT_FOUND
 
 
 async def test_health_check(client):
@@ -46,3 +47,14 @@ async def test_health_ping(client):
     response = await client.get("/health/ping")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"ping": "pong"}
+
+
+async def test_health_catch_all(verify_404):
+    """Test the helth catch-all endpoint for 404 response."""
+
+    unknown_path = "some/random/path"
+    await verify_404(
+        f"/health/{unknown_path}",
+        ERROR_HEALTH_WEBSERVICE_NOT_FOUND,
+        f"Webservice /health/{unknown_path} requested not found.",
+    )
